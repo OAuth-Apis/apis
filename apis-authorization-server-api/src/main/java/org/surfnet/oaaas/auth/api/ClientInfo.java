921312bab53659c99d674fb802768a2f2b56fc8d
Todd Fast <todd@digitalexistence.com>
2013-04-11 17:09:47 -0700

    Added JavaDoc

--- apis-authorization-server-api/src/main/java/org/surfnet/oaaas/auth/api/ClientInfo.java
+++ apis-authorization-server-api/src/main/java/org/surfnet/oaaas/auth/api/ClientInfo.java
@@ -4,7 +4,16 @@ import java.util.List;
 import java.util.Map;
 
 /**
+ * Information about the client application requesting authorization.
+ * Represents a Client as defined by the OAuth 2 specification:
  *
+ * <pre>
+ *         An application making protected resource requests on behalf of the resource owner and with its
+ *         authorization.  The term client does not imply any particular implementation characteristics (e.g. whether
+ *         the application executes on a server, a desktop, or other devices).
+ * </pre>
+ * 
+ * @see org.surfnet.oaaas.model.Client
  * @author Todd Fast
  */
 public interface ClientInfo {
