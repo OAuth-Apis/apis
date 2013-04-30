1d0bf63e84b63ee3155bab337c57399d597effde
oharsta <oharsta@zilverline.com>
2013-04-30 14:04:35 +0200

    New compass project

---
+++ config.rb
@@ -0,0 +1,8 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
