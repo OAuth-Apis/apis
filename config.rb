06e246c463d68716caf51f33a4e7d2d18f96ad03
oharsta <oharsta@zilverline.com>
2013-05-01 11:49:21 +0200

    Presentation WIP

--- config.rb
+++ config.rb
@@ -6,3 +6,4 @@ css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
+
---
+++ javascripts/apis.js
@@ -0,0 +1,12 @@
+$(function(){
+	Reveal.initialize({
+				controls: true,
+				progress: true,
+				history: true,
+				center: true,
+				width: "100%",
+	    height: "100%",
+	    transition: "zoom",
+	});
+});
+
