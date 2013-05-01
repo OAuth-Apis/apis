a25e775966db7be638d22ef489c9513ab9c5c29b
oharsta <oharsta@zilverline.com>
2013-05-01 16:24:08 +0200

    Presentation APIS WIP

--- javascripts/apis.js
+++ javascripts/apis.js
@@ -1,12 +1,13 @@
-$(function(){
-	Reveal.initialize({
-				controls: true,
-				progress: true,
-				history: true,
-				center: true,
-				width: "100%",
-	    height: "100%",
-	    transition: "zoom",
-	});
+$(function () {
+  Reveal.initialize({
+    controls: true,
+    progress: true,
+    //history: true,
+    center: true,
+    width: "100%",
+    height: "100%",
+    transition: "fade",
+    transitionSpeed: "slow"
+  });
 });
 
