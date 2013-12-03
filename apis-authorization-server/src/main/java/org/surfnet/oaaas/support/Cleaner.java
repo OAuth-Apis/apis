9be18eeff4c905f218bb01261c1469d25e7882e1
Geert van der Ploeg <geert.vanderploeg@flex.surfnet.nl>
2013-12-03 13:31:32 +0100

    Proper constants for timing parameters

--- apis-authorization-server/src/main/java/org/surfnet/oaaas/support/Cleaner.java
+++ apis-authorization-server/src/main/java/org/surfnet/oaaas/support/Cleaner.java
@@ -9,18 +9,29 @@ import org.surfnet.oaaas.repository.AccessTokenRepository;
 import javax.inject.Inject;
 import java.util.Date;
 
+/**
+ * Helper class that contains scheduled tasks for database cleanup
+ */
 public class Cleaner {
   private static final Logger LOG = LoggerFactory.getLogger(Cleaner.class);
 
   @Inject
   private AccessTokenRepository accessTokenRepository;
 
-  private static final int EXPIRE = 1000 * 10;
+  /**
+   * Interval in ms between cleanup jobs
+   */
+  private static final long CLEANUP_INTERVAL = 1000 * 3600;
 
-  @Scheduled(fixedDelay = EXPIRE)
+  /**
+   * Throw away expired tokens after 30 days
+   */
+  private static final long EXPIRED_TOKEN_CLEANUP_AGE = 1000L * 3600 * 24 * 30;
+
+  @Scheduled(fixedDelay = CLEANUP_INTERVAL)
   public void cleanupExpiredAccessTokens() {
     LOG.debug("Cleaning up expired access tokens");
-    for (AccessToken at : accessTokenRepository.findByMaxExpires(System.currentTimeMillis() - 1000)) {
+    for (AccessToken at : accessTokenRepository.findByMaxExpires(System.currentTimeMillis() - EXPIRED_TOKEN_CLEANUP_AGE)) {
       LOG.debug("Deleting expired access token {} (created: {}, expired: {})", at.getToken(), at.getCreationDate(), new Date(at.getExpires()));
       accessTokenRepository.delete(at);
     }
