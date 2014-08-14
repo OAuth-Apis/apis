9de8a68d1eeb248f54acf46abcc2540c2d6f8028
Lars Vonk <lars.vonk@gmail.com>
2014-08-14 15:32:43 +0200

    Fixed test to work with spring boot.

--- apis-authorization-server-boot/src/test/java/org/surfnet/oaaas/config/SpringConfigTest.java
+++ apis-authorization-server-boot/src/test/java/org/surfnet/oaaas/config/SpringConfigTest.java
@@ -19,6 +19,7 @@ package org.surfnet.oaaas.config;
 import com.googlecode.flyway.core.Flyway;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.ContextConfiguration;
@@ -34,17 +35,16 @@ import static junit.framework.Assert.assertTrue;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
-@ContextConfiguration(classes = SpringConfiguration.class)
+@SpringApplicationConfiguration(classes = SpringConfiguration.class)
 public class SpringConfigTest {
 
-  @Inject
+  @Autowired
   DataSource dataSource;
 
-  @Inject
+  @Autowired
   Flyway flyway;
 
-  @Inject
+  @Autowired
   VerifyResource verifyResource;
 
 
