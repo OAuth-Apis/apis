0cda1e767a84ab29f066821afa79d5e7785d89f0
Andrea Biancini <andrea.biancini@gmail.com>
2015-07-16 07:38:27 +0000

    Added jetty connector to permit apache https proxying of shibboleth authentication

--- jetty-connector/src/main/java/org/surfnet/oaaas/jetty/SelectChannelConnectorHttps.java
+++ jetty-connector/src/main/java/org/surfnet/oaaas/jetty/SelectChannelConnectorHttps.java
@@ -1,5 +1,6 @@
 package org.surfnet.oaaas.jetty;
 
+import java.util.Enumeration;
 import java.io.IOException;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.nio.SelectChannelConnector;
@@ -10,11 +11,22 @@ public class SelectChannelConnectorHttps extends SelectChannelConnector {
 
 	public static final String X_FORWARDED_PROTO = "x-forwarded-proto";
 
+	private String getHeaderCaseInsensitive(Request request, String headerName) {
+		Enumeration<String> headerNames = (Enumeration<String>) request.getHeaderNames();
+		while (headerNames.hasMoreElements()){
+			String curHeaderName = (String) headerNames.nextElement();
+			if (curHeaderName.toLowerCase().equals(headerName)) {
+				return request.getHeader(curHeaderName);
+			}
+		}
+		return null;
+	}
+
 	public void customize(EndPoint endpoint, Request request) throws IOException {
 
-		String forwardedProtocol = request.getHeader(X_FORWARDED_PROTO);
+		String forwardedProtocol = getHeaderCaseInsensitive(request, X_FORWARDED_PROTO);
 		if (forwardedProtocol != null) {
-			if (forwardedProtocol.indexOf("https") != 0) {
+			if (forwardedProtocol.indexOf("https") >= 0) {
 				request.setScheme("https");
 			}
 		}
