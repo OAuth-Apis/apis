759a9ee0ffa623b1c1e1fd23d9e708bdb1ad6cd5
plovekar <plovekar@zebra.com>
2016-03-03 12:27:57 -0600

    Fixes minor issues with a seed data script in the OSS

--- apis-authorization-server/src/test/resources/db/migration/mysql/V1__auth-server-admin.sql
+++ apis-authorization-server/src/test/resources/db/migration/mysql/V1__auth-server-admin.sql
@@ -1,13 +1,13 @@
 /*
 Local administration application
  */
-INSERT INTO resourceserver (id, contactEmail,  contactName, name, key0, secret, owner, thumbNailUrl)
+INSERT INTO resourceserver (id, contactEmail,  contactName, resourceServerName, resourceServerKey, secret, owner, thumbNailUrl)
 VALUES
 	(99998, 'localadmin@example.com','local admin','Authorization Server Apis',
 	'authorization-server-admin', 'cafebabe-cafe-babe-cafe-babecafebabe', null, 'https://static.surfconext.nl/media/logo-surfnet-small.png');
 INSERT INTO Resourceserver_scopes values (99998, 'read'),(99998, 'write') ;
 
-INSERT INTO client (id, contactEmail, contactName, description, name, thumbNailUrl, resourceserver_id,
+INSERT INTO client (id, contactEmail, contactName, description, clientName, thumbNailUrl, resourceserver_id,
 clientId, secret)
 VALUES
     (99998, 'client@coolapp.com', 'john.doe', 'Javascript application for authorization server administration',
